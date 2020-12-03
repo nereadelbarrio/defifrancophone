f03e1229932258e1b716b2961babe9cbbf2bcf1c
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2020-12-03 13:41:22 +0100

    Update style.js

--- style.js
+++ style.js
@@ -57,6 +57,8 @@
 				{ name: 'Caja 7', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-7' } },
 				{ name: 'Caja 8', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-8' } },
 				{ name: 'Caja 9', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-9' } },
+				{ name: 'Caja 10', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-10' } },
+
 
 				{ name: 'Icono Vídeo', element: 'span', attributes: { 'class': 'icon icon-video' } },
 				{ name: 'Icono Cesta letras', element: 'span', attributes: { 'class': 'icon icon-cesta-letras' } },
